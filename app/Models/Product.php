cc809abbbd7654765ea563d2602e6252beb06f19
YASHIOKENYA05 <yashiokenya05@YASHIOKENYA05noMacBook-Air.local>
2023-07-24 14:29:18 +0300

    Declare one to many relationship using eloquent's belongsTo method

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,15 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     use HasFactory;
-
+    protected $table='product';
     //define an inverse relationship to the category model
 
-    public function category()
+    public function category() : BelongsTo
     {
-        return this-> belongsTo(User::class);
+        return this-> belongsTo(Category::class);
     }
 }
