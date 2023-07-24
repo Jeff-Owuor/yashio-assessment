f82a74bf46bb6999d8243099719811d584c1194a
YASHIOKENYA05 <yashiokenya05@YASHIOKENYA05noMacBook-Air.local>
2023-07-24 17:05:25 +0300

    Display all products on the page

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,7 +1,29 @@
 @extends("layout")
 
 @section("content")
-
-<h1>Products</h1>
-<h2></h2>
+  <div class="container">
+    <h1>All Products</h1>
+    <a href="{{url ('/product/create')}}">
+        <span class="material-symbols-outlined">
+add
+</span>
+</a>
+  <div class="row">
+  @foreach ($products as $product)
+  <div class="col-md-3 mt-3">
+  <div class="card">
+  <!-- <div class="card-header">
+    Quote
+  </div> -->
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p>{{$product->name}}</p>
+      <p>{{$product->price}}</p>
+    </blockquote>
+  </div>
+</div>
+</div>
+  @endforeach
+  </div>
+  </div>
 @endsection
