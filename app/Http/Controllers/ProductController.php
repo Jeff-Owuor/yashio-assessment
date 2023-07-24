2813bd877276fc8000afbf5cb1c01bce4d6c9a76
YASHIOKENYA05 <yashiokenya05@YASHIOKENYA05noMacBook-Air.local>
2023-07-24 17:02:50 +0300

    Define function to save products to the database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
-
+use App\Models\Category;
 
 class ProductController extends Controller
 {
@@ -13,5 +13,29 @@ class ProductController extends Controller
     {
         $product = Product::all();
         return view('products',["products"=>$product]);
+    }
+    
+
+    public function create()
+    {
+        // Get all roles to populate the dropdown list
+        $categories = Category::all();
+        return view('productForm', ['categories' => $categories]);
+    }
+
+    public function store (Request $request)
+    {
+        $product = new Product();
+        $validatedData = $request->validate([
+            'name' => 'required | string | max:100',
+            'price' => 'required | max:50'
+          ]);
+          $product->name = $validatedData['name'];
+          $product->price = $validatedData['price'];
+          $product->category_id=$request->input('category_id');
+  
+          $product->save();
+  
+          return redirect()->to('/')->with('success', 'Product created successfully!');
     }
 }
