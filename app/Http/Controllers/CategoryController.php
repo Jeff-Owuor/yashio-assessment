298e287808100bcbde0952bdb05e75dc1bb8af2e
YASHIOKENYA05 <yashiokenya05@YASHIOKENYA05noMacBook-Air.local>
2023-07-24 17:01:58 +0300

    Display all categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,7 +7,13 @@ use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+
+    public function allCategories ()
+    {
+        $category = Category::all();
+        return view('welcome',["categories"=>$category]);
+    }
+
     public function store(Request $request)
     {
         // validate the incoming data
@@ -19,7 +25,7 @@ class CategoryController extends Controller
 
         $category->save();
 
-        return redirect()->to('/')->with('success', 'Product created successfully!');
+        return redirect()->to('/')->with('success', 'Category created successfully!');
     }
 
 }
