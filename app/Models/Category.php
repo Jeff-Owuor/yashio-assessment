979fbb5713d94941e95347f76537e58be5c32407
YASHIOKENYA05 <yashiokenya05@YASHIOKENYA05noMacBook-Air.local>
2023-07-24 14:28:17 +0300

    Declare many to one relationship using eloquent's hasMany method

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,13 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-   
+    protected $table='category';
    // this function defines the one to many relationship with the products model
-   public function products(){
-    return this->hasMany(Products::class);
+   public function products() : HasMany
+   {
+
+    return this->hasMany(Product::class);
    }
 
     use HasFactory;
