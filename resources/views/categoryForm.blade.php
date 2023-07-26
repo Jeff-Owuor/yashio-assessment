15a88aff30d33ff4d4de1904589eed2f41074016
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-07-26 11:27:34 +0300

    Center category addition form

--- resources/views/categoryForm.blade.php
+++ resources/views/categoryForm.blade.php
@@ -2,15 +2,23 @@
 
 
 @section("content")
-
-<h1>Create a new category</h1>
+<div class="container">
+<h1 style="text-align:center;">Create a new category</h1>
+<div class="row">
+    <div class="col-md-4"></div>
+    <div class="col-md-4">
 <form method="post" action="{{ route('form.submit') }}">
     @csrf 
-    <label for="name">
+    <label for="name" style="font-size:35px;">
         Name:
     </label>
-    <input  type="text" name="name" id="name" required/>
-    <button type="submit">Submit</button>
+    <input  type="text" name="name" id="name" required  
+    style="display:block; width:60%;"
+    placeholder="Enter category name"
+    />
+    <button type="submit" class="btn btn-secondary mt-2">Submit</button>
 </form>
-
+</div>
+</div>
+</div>
 @endsection
