4fcce97d94957bfc6abd1091686d5cc7c1fc6c54
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-07-27 13:11:51 +0300

    Define edit function that sends data to the form
    define update category that updates a category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -33,5 +33,20 @@ class CategoryController extends Controller
 
         return redirect()->to('/')->with('success', 'Category created successfully!');
     }
+    public function edit($id)
+    {
+        $category = Category::find($id);
+        return view('updateCategoryForm',compact('category','id'));
+    }
 
+    public function updateCategory(Request $request,$id)
+    {
+        $category = Category::find($id);
+        $validatedData = $request->validate([
+            'name' => 'required | string | max:100',
+          ]);
+        $category->name = $validatedData['name'];
+        $category->update();
+        return redirect()->to('/')->with('success', 'Category updated successfully!');
+    }
 }
